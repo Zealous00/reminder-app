ccdf607421601fae0a7cdbdb091288a6d207ccc0
Zealous00 <dfekete00@gmail.com>
2023-02-01 23:07:30 +0200

    profilescreen layout improvement

--- app/src/main/java/com/fekete/david/reminderapp/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/fekete/david/reminderapp/ui/profile/ProfileScreen.kt
@@ -1,27 +1,32 @@
 package com.fekete.david.reminderapp.ui.profile
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.style.TextOverflow.Companion.Clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.sp
 import com.fekete.david.reminderapp.R
+import org.w3c.dom.Text
 
 @Composable
 fun ProfileScreen(
@@ -43,43 +48,136 @@ fun ProfileScreen(
             Column(
                 modifier = Modifier.fillMaxSize(),
             ) {
-                Column(
+                PicturePart(
                     modifier = Modifier
                         .fillMaxWidth()
                         .background(color = MaterialTheme.colors.primary)
-                        .weight(0.7f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    OutlinedButton(
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier.size(150.dp),
-                        shape = CircleShape,
-                        border = BorderStroke(5.dp, Color.Black),
-                        contentPadding = PaddingValues(0.dp),
-                        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.White)
-                    ) {
-                        Icon(
-                            painter = rememberVectorPainter(image = Icons.Rounded.Person),
-//                            tint = Color.Black,
-                            contentDescription = stringResource(R.string.account),
-                            modifier = Modifier.size(100.dp)
-                        )
-                    }
-
-                }
+                        .weight(0.5f)
+                )
                 Spacer(modifier = Modifier.height(8.dp))
-                Column(
+                DataPart(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .background(color = Color.Red)
-                        .weight(1f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(text = "This is the profile screen second part my friend")
-                }
+//                        .background(color = Color.Red)
+                        .weight(1f)
+                )
             }
         }
+    }
+}
+
+
+@Composable
+fun PicturePart(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(150.dp),
+            shape = CircleShape,
+            border = BorderStroke(5.dp, Color.Black),
+            contentPadding = PaddingValues(0.dp),
+            colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.White)
+        ) {
+            Icon(
+                painter = rememberVectorPainter(image = Icons.Rounded.Person),
+//                            tint = Color.Black,
+                contentDescription = stringResource(R.string.account),
+                modifier = Modifier.size(100.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun DataPart(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.4f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                shape = RoundedCornerShape(corner = CornerSize(50.dp)),
+                border = BorderStroke(2.dp, color = MaterialTheme.colors.primary)
+            ) {
+                Text(
+//                color = Color.Black,
+                    text = "Profile details",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = "Nickname",
+                fontSize = 14.sp,
+                modifier = Modifier.padding(4.dp)
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                shape = RoundedCornerShape(corner = CornerSize(50.dp)),
+                border = BorderStroke(2.dp, color = MaterialTheme.colors.primary)
+            ) {
+                Text(
+//                color = Color.Black,
+                    text = "This is the nickname",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Phone number",
+                fontSize = 14.sp,
+                modifier = Modifier.padding(4.dp)
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                shape = RoundedCornerShape(corner = CornerSize(50.dp)),
+                border = BorderStroke(2.dp, color = MaterialTheme.colors.primary)
+            ) {
+                Text(
+//                color = Color.Black,
+                    text = "This is the phone number",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+
+        }
+
+
     }
 }
