062b9cb6bfdcbbbecd7c66fe3eab211832e0b41d
Zealous00 <dfekete00@gmail.com>
2023-02-17 00:53:14 +0200

    rendering of priority added

--- app/src/main/java/com/fekete/david/reminderapp/ui/home/Reminders.kt
+++ app/src/main/java/com/fekete/david/reminderapp/ui/home/Reminders.kt
@@ -1,10 +1,12 @@
 package com.fekete.david.reminderapp.ui.home
 
 import android.os.Bundle
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -13,6 +15,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -20,6 +24,7 @@ import androidx.core.os.bundleOf
 import androidx.navigation.NavArgumentBuilder
 import androidx.navigation.NavController
 import androidx.navigation.navArgument
+import com.fekete.david.reminderapp.data.entitiy.Priority
 import com.fekete.david.reminderapp.data.entitiy.Reminder
 import com.fekete.david.reminderapp.viewmodel.AuthViewModel
 import com.fekete.david.reminderapp.viewmodel.ReminderViewModel
@@ -91,7 +96,7 @@ private fun ReminderListItem(
     ConstraintLayout(modifier = Modifier
         .fillMaxWidth()
         .clickable { onClick() }) {
-        val (divider, text, date) = createRefs()
+        val (divider, text, date, circle, priorityText) = createRefs()
         Divider(
             Modifier.constrainAs(divider) {
                 top.linkTo(parent.top)
@@ -99,15 +104,38 @@ private fun ReminderListItem(
                 width = Dimension.fillToConstraints
             }
         )
+        Box(
+            modifier = Modifier
+                .size(25.dp)
+                .clip(CircleShape)
+                .background(choosePriorityColor(reminder.priority))
+                .constrainAs(circle) {
+                    top.linkTo(divider.bottom, margin = 12.dp)
+                    start.linkTo(divider.start, margin = 20.dp)
+                }
+        )
+        Text(
+            text = reminder.priority.name,
+            maxLines = 1,
+            color = choosePriorityColor(reminder.priority),
+            style = MaterialTheme.typography.subtitle2,
+            modifier = Modifier.constrainAs(priorityText) {
+                linkTo(
+                    start = circle.start,
+                    end = circle.end,
+                )
+                top.linkTo(circle.bottom, margin = 2.dp)
+            }
+        )
         Text(
             text = reminder.message,
-            maxLines = 1,
+            maxLines = 2,
             style = MaterialTheme.typography.subtitle1,
             modifier = Modifier.constrainAs(text) {
                 linkTo(
-                    start = parent.start,
+                    start = circle.end,
                     end = parent.end,
-                    startMargin = 0.dp,
+                    startMargin = 16.dp,
                     endMargin = 0.dp
                 )
                 top.linkTo(parent.top, margin = 10.dp)
@@ -120,7 +148,7 @@ private fun ReminderListItem(
             style = MaterialTheme.typography.subtitle2,
             modifier = Modifier.constrainAs(date) {
                 linkTo(
-                    start = parent.start,
+                    start = circle.end,
                     end = parent.end,
                     startMargin = 0.dp,
                     endMargin = 0.dp
@@ -136,4 +164,13 @@ private fun ReminderListItem(
 private fun Date.formatToString(): String {
     return SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault()).format(this)
 
+}
+
+private fun choosePriorityColor(p: Priority): Color {
+    return when (p) {
+        Priority.HIGH -> Color.Red
+        Priority.MEDIUM -> Color.Yellow
+        Priority.LOW -> Color.Green
+        else -> Color.White
+    }
 }
